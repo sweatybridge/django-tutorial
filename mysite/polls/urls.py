ed312f4c80b12a953389a7a8cfdf649fd58659f6
Chng Nai Yun <naiyun.chng@od-eon.com>
2013-01-21 16:04:41 +0800

    removed redundant import in polls/urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.views.generic import DetailView, ListView
 from polls.models import Poll
 
